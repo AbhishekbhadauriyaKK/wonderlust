845678196ba3708e872867db28c28a9426a22fa4
ABHISHEK BHADAURIYA <112882023+AbhishekbhadauriyaKK@users.noreply.github.com>
2024-10-23 16:14:01 +0530

    Delete utils directory

--- utils/expresserror.js
+++
@@ -1,8 +0,0 @@
-class expresserror extends Error{
-    constructor (statuscode , message){
-        super();
-        this.statuscode=statuscode;
-        this.message=message;
-    }
-}
-module.exports=expresserror;
